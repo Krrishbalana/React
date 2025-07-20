c68299ce95be1647f16a59029e72100c6cf23a68
Krrishbalana <krrishbalana@gmail.com>
2025-07-20 20:39:22 +0530

    background color changer project using react and tailwind

--- 06-Bg-Color-changer/src/App.jsx
+++ 06-Bg-Color-changer/src/App.jsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+const [background, setBackground] = useState('');
+const blue = function(){
+  setBackground("blue");
+}
+const red = function(){
+  setBackground("red");
+}
+const pink = function(){
+  setBackground("pink");
+}
+const yellow = function(){
+  setBackground("yellow");
+}
+const olive = function(){
+  setBackground("olive");
+}
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='relative h-screen w-screen' style={{backgroundColor: background}}>
+        <div className='absolute rounded-4xl left-1/2 -translate-x-1/2 top-1 shadow-2xl shadow-neutral-950 flex space-x-5 justify-center bg-black text-white'>
+            <button onClick={blue} className='border px-4 m-4 rounded-2xl text-2xl hover:bg-blue-700 hover:text-black hover:px-6 duration-500'>blue</button>
+            <button onClick={red} className='border px-4 m-4 rounded-2xl text-2xl hover:bg-red-700 hover:text-black hover:px-6 duration-500'>red</button>
+            <button onClick={pink} className='border px-4 m-4 rounded-2xl text-2xl hover:bg-pink-300 hover:text-black hover:px-6 duration-500'>pink</button>
+            <button onClick={yellow} className='border px-4 m-4 rounded-2xl text-2xl hover:bg-yellow-500 hover:text-black hover:px-6 duration-500'>yellow</button>
+            <button onClick={olive} className='border px-4 m-4 rounded-2xl text-2xl hover:bg-lime-900 hover:text-black hover:px-6 duration-500'>olive</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
